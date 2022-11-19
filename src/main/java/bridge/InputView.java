8ac3dfa4283edf1a95c9602fb219427fefe8b626
linda2927 <linda2927@naver.com>
2022-11-19 18:49:56 +0900

    feat: receive user input of bridge length and validates it

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,4 +1,5 @@
 package bridge;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -8,8 +9,28 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        System.out.println("다리 길이를 입력해주세요.");
+        String bridgeLengthInput = Console.readLine();
+        return bridgeLengthInput;
+    }
+
+    /**
+     * Validates user input of bridge length
+     * @param userInput string format of user input
+     * @return int format of valid bridge length
+     */
+    public int validateBridgeSizeInput(String userInput) throws IllegalArgumentException{
+        int bridgeLength;
+        try {
+            bridgeLength = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자여야 합니다.");
+        }
+        if (bridgeLength < 3 || bridgeLength > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 3과 20 사이의 정수여야 합니다.");
+        }
+        return bridgeLength;
     }
 
     /**
