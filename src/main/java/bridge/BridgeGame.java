67785a1d23b2debbf5f8032d9ac15aa9e9364a0e
linda2927 <linda2927@naver.com>
2022-11-19 21:40:09 +0900

    feat: receive move command from user and compare with bridge answer

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -9,15 +9,15 @@ import java.util.List;
 public class BridgeGame {
 
     private List<String> bridgeAnswer;
-    private List<String> inputNumbers;
     private int attempts;
     private final InputView inputView;
+    private final OutputView outputView;
 
     public BridgeGame() {
         this.bridgeAnswer = new ArrayList<>();
-        this.inputNumbers = new ArrayList<>();
         this.attempts = 0;
         this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     /**
@@ -37,11 +37,32 @@ public class BridgeGame {
     }
 
     /**
+     * Compare user move command input and bridge answer
+     * @param answer nth element of bridge answer
+     * @param userMoveCommand user input of move command
+     */
+    public String calculateResult(String answer, String userMoveCommand) {
+        if (answer.equals(userMoveCommand)) { return "O"; }
+        return "X";
+    }
+
+    /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @return "O" or "X" according to the {@link BridgeGame#calculateResult(String, String)}
      */
-    public void move() {
+    public String move(String answer) {
+        String moveCommand = "";
+        try {
+            moveCommand = this.inputView.readMoving();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String result = calculateResult(answer, moveCommand);
+        outputView.addAnswer(moveCommand, result);
+        return result;
     }
 
     /**
