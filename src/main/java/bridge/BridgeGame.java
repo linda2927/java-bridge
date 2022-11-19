2e557a83c02632611c0d63bd6723e673fd0b998a
linda2927 <linda2927@naver.com>
2022-11-19 20:03:30 +0900

    feat: make bridge and set bridge answer for BridgeGame instance

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,40 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private List<String> bridgeAnswer;
+    private List<String> inputNumbers;
+    private int attempts;
+    private final InputView inputView;
+
+    public BridgeGame() {
+        this.bridgeAnswer = new ArrayList<>();
+        this.inputNumbers = new ArrayList<>();
+        this.attempts = 0;
+        this.inputView = new InputView();
+    }
+
+    /**
+     * Set bridge answer for BridgeGame instance
+     */
+    public void setBridgeAnswer() {
+        int bridgeLength = 0;
+        while (bridgeLength == 0) {
+            try {
+                bridgeLength = inputView.readBridgeSize();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridgeAnswer = bridgeMaker.makeBridge(bridgeLength);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
