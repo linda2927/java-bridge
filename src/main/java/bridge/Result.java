b0e945f09f54e139596c5ba326a95d4030c01f13
linda2927 <linda2927@naver.com>
2022-11-21 08:19:34 +0900

    feat: add result element ("O" or "X") and clear result list on retry action

--- src/main/java/bridge/Result.java
+++ src/main/java/bridge/Result.java
@@ -27,4 +27,28 @@ public class Result {
     public List<String> getLowerResult() {
         return lowerResult;
     }
+
+    /**
+     * Add result string "O" or "X" to upper and lower result list
+     * @param moveCommand user's move command
+     * @param result whether user's guess was correct (in "O" or "X")
+     */
+    public void addResult(String moveCommand, String result) {
+        if (moveCommand.equals("U")) {
+            upperResult.add(result);
+            lowerResult.add(" ");
+        }
+        if (moveCommand.equals("D")) {
+            lowerResult.add(result);
+            upperResult.add(" ");
+        }
+    }
+
+    /**
+     * Clears the result list in case of game retry
+     */
+    public void clearAnswer() {
+        this.upperResult = new ArrayList<>();
+        this.lowerResult = new ArrayList<>();
+    }
 }
