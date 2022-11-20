2a6d6df28154d727230c8df8f5947d1b71de5dc3
linda2927 <linda2927@naver.com>
2022-11-21 08:30:09 +0900

    feat: prints the result or the final result of the game

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,43 @@
 package bridge;
 
+import java.util.List;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
     /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * Prints the result of guessing so far line by line
+     * @param result instance of Result class which contains the result of guessing
      */
-    public void printMap() {
+    public void printMap(Result result) {
+        printLine(result.getUpperResult());
+        printLine(result.getLowerResult());
     }
 
     /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * Prints upper or lower bridge answer
+     * @param partialBridge upper or lower bridge answer
      */
-    public void printResult() {
+    private void printLine(List<String> partialBridge) {
+        StringBuilder lineStr = new StringBuilder("[ ");
+        for (int i = 0; i < partialBridge.size(); i++) {
+            if (i == partialBridge.size() - 1) {
+                lineStr.append(partialBridge.get(i)).append(" ]");
+                break;
+            }
+            lineStr.append(partialBridge.get(i)).append(" | ");
+        }
+
+        System.out.println(lineStr);
+    }
+
+    /**
+     * Prints the final result
+     * @param result instance of Result class which contains the result of guessing
+     */
+    public void printResult(Result result) {
+        System.out.println("최종 게임 결과");
+        printMap(result);
     }
 }
