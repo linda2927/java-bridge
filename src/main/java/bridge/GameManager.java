11ac33faa9a13f47e433b80ce5cea1abebb2f9be
linda2927 <linda2927@naver.com>
2022-11-21 08:27:18 +0900

    feat: add methods to print result

--- src/main/java/bridge/GameManager.java
+++ src/main/java/bridge/GameManager.java
@@ -62,4 +62,26 @@ public class GameManager {
         }
         return gameCommand;
     }
+
+    /**
+     * Prints the result of a single guess
+     * @param result instance of Answer class which includes
+     */
+    public void guessResult(Result result) {
+        outputView.printMap(result);
+    }
+
+    /**
+     * Prints the final result of the game
+     * @param result instance of Result class which stores the list of single guess results
+     * @param isSuccess whether user has succeeded to guess the whole bridge answer
+     * @param attempts number of tries
+     */
+    public void finalResult(Result result, boolean isSuccess, int attempts) {
+        outputView.printResult(result);
+        String resultStatus = "성공";
+        if (!isSuccess) { resultStatus = "실패"; }
+        System.out.println("\n게임 성공 여부: " + resultStatus);
+        System.out.println("총 시도한 횟수: " + attempts);
+    }
 }
