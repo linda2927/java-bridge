5753cd63ceab313020f3332b4d57155342892e09
linda2927 <linda2927@naver.com>
2022-11-21 09:18:34 +0900

    feat: add retry and quit method

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -43,10 +43,21 @@ public class BridgeGame {
     }
 
     /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * Increase attempt count and re-runs the guessing process
      */
     public void retry() {
+        attempts++;
+        this.result.clearAnswer();
+        play();
     }
+
+    /**
+     * Prints final result and stops the game
+     * @param isSuccess whether user has succeeded in guessing the bridge
+     */
+    public void quit(boolean isSuccess) {
+        manager.finalResult(this.result, isSuccess, attempts);
+    }
+
+    public void play() {}
 }
